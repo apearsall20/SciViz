11bf9113cba3fe5a3623ab6882d9ed73b1b76c5e
Andrew Pearsall <apearsall20@gmail.com>
2021-12-12 19:09:48 -0600

    done with arrows

--- main.js
+++ main.js
@@ -3,9 +3,18 @@ height = 500;
 
 async function first() {
   
-  weather = await d3.csv("https://raw.githubusercontent.com/apearsall20/SciViz/master/data/weather.csv");
+  weather = await d3.csv("https://raw.githubusercontent.com/apearsall20/SciViz/master/data/grid.csv");
   maps_temp = await d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson");
   maps = maps_temp.features;
+
+  temperature = [];
+  for (thing in weather){
+    row = {}
+    row.x = weather[thing].longitude
+    row.y = weather[thing].latitude
+    row.value = weather[thing].temperature
+    temperature.push(row)
+  };
 
 	var svg = d3.select("#Viz");
 
@@ -16,7 +25,8 @@ async function first() {
     .scale(770)                       // This is like the zoom
     .translate([ width/2, height/2 ])
 
-
+  thresholds = d3.range(1, 20).map(i => -40+3.25*i)  
+  console.log(thresholds);
     // Draw the map
     svg.append("g")
         .selectAll("path")
@@ -29,15 +39,14 @@ async function first() {
           )
        .style("stroke", "black");
 
-    console.log(Number(weather[0].longitude), 
-      Number(weather[0].u),
-      Number(weather[0].longitude) + Number(weather[0].u),
-      Number(weather[0].latitude),
-      Number(weather[0].v),
-      Number(weather[0].latitude) + Number(weather[0].v),
-      projection([Number(weather[0].longitude) + Number(weather[0].u),
-        Number(weather[0].latitude) + Number(weather[0].v)])
-      )
+  contours = d3.contourDensity()
+    .x(function(d) {return d.x })
+    .y(function(d) {return d.y })
+    .size([width,height])
+    .thresholds(thresholds)
+  (temperature);
+
+    console.log(contours)
 
 
     svg.append("g")
@@ -45,11 +54,11 @@ async function first() {
         .data(weather)
         .enter()
         .append("circle")
-          .attr("cx", function(d){ return projection([d.longitude, d.latitude])[0] })
-          .attr("cy", function(d){ return projection([d.longitude, d.latitude])[1] })
-          .attr("r", 1)
+          .attr("cx", function(d){ return projection([d.lon2, d.lat2])[0] })
+          .attr("cy", function(d){ return projection([d.lon2, d.lat2])[1] })
+          .attr("r", 1.1)
+          .attr("fill","black")
 
-    console.log(projection((weather[0].longitude,weather[0].latitude)))
     svg.append("g")
         .selectAll("line")
         .data(weather)
@@ -61,13 +70,15 @@ async function first() {
         .attr("y2", function(d){ return projection([d.lon2, d.lat2])[1] })
         .style("stroke","black")
 
-/*        .attr("x1", function(d){ return (d.longitude)})
-        .attr("y1", function(d){ return (d.latitude)})
-        .attr("x2", function(d){return (d.longitude) + d.u})
-        .attr("y2", function(d){return (d.latitude) + d.v})
-        .attr("d",d3.geoPath()
-          .projection(projection))
-*/
+    svg.append("g")
+      .selectAll("path")
+      .data(contours)
+      .enter()
+      .append("path")
+        .attr("d", d3.geoPath())
+        .attr("fill","none")
+        .attr("stroke","pink")
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
